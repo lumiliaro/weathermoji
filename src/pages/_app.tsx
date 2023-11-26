de756d765960ec421a605430b7d8fe9ca1f1e2c6
12ukas <roehrlukas@icloud.com>
2023-11-26 12:45:28 +0100

    mantine colors + dark theme enablend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,16 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import "@mantine/core/styles.css";
 import { MantineProvider, createTheme } from "@mantine/core";
+import { mantineVioletColors } from "~/utils/styles";
 
 const theme = createTheme({
   /** Put your mantine theme override here */
+  colors: {mantineVioletColors}
 });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider theme={theme} defaultColorScheme="dark">
       <Component {...pageProps} />
     </MantineProvider>
   );
---
+++ src/utils/styles.ts
@@ -0,0 +1,14 @@
+import { type MantineColorsTuple } from "@mantine/core";
+
+export const mantineVioletColors: MantineColorsTuple = [
+  "#f6ecff",
+  "#e7d6fb",
+  "#caabf1",
+  "#ac7ce8",
+  "#9354e0",
+  "#833cdb",
+  "#7b2eda",
+  "#6921c2",
+  "#5d1cae",
+  "#501599"
+]
