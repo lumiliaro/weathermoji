36ca935f741f100d619517c2dd550c8c8039f092
12ukas <roehrlukas@icloud.com>
2023-11-27 20:50:23 +0100

    extended api calls + types

--- src/server/api/routers/types.ts
+++ src/server/api/routers/types.ts
@@ -23,3 +23,27 @@ export type OpenMeteoGeoCodingLocation = {
     admin4: string;
   }[];
 };
+
+export type OpenMeteoGeoCodingForecast = {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  daily_units: {
+    time: string;
+    weather_code: string;
+    uv_index_max: string;
+    uv_index_clear_sky_max: string;
+    precipitation_probability_max: string;
+  };
+  daily: {
+    time: string[];
+    weather_code: number[];
+    uv_index_max: number[];
+    uv_index_clear_sky_max: number[];
+    precipitation_probability_max: (number | null)[];
+  };
+};
--- src/server/api/routers/weather.ts
+++ src/server/api/routers/weather.ts
@@ -1,19 +1,17 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { type OpenMeteoGeoCodingLocation } from "./types";
-
-const weather = {
-  id: 1,
-  name: "Hello World",
-};
+import {
+  type OpenMeteoGeoCodingLocation,
+  type OpenMeteoGeoCodingForecast,
+} from "./types";
 
 export const weatherRouter = createTRPCRouter({
   getLocations: publicProcedure
-    .input(z.object({ location: z.string() }))
+    .input(z.object({ location: z.string().trim() }))
     .query(async ({ input }) => {
       if (input.location.length < 3) {
-        return [];
+        return;
       }
 
       const response = await fetch(
@@ -22,28 +20,38 @@ export const weatherRouter = createTRPCRouter({
       const locations = (await response.json()) as OpenMeteoGeoCodingLocation;
 
       if (!locations) {
-        return [];
+        return;
       }
 
       return locations.results;
     }),
 
   getForecast: publicProcedure
-    .input(z.object({ location: z.string(), days: z.number() }))
+    .input(
+      z.object({
+        latitude: z.string().optional(),
+        longitude: z.string().optional(),
+        days: z.number(),
+      }),
+    )
     .query(async ({ input }) => {
-      if (input.location.length < 3) {
-        return [];
+      if (input.latitude === undefined || input.longitude === undefined) {
+        return;
       }
 
       if (input.days < 1) {
-        return [];
+        return;
       }
 
       const response = await fetch(
-        `https://geocoding-api.open-meteo.com/v1/search?name=${input.location}&count=5&language=en&format=json`,
+        `https://api.open-meteo.com/v1/forecast?latitude=${input.latitude}&longitude=${input.longitude}&daily=weather_code,precipitation_probability_max&timezone=auto&models=best_match&forecast_days=${input.days}`,
       );
-      const locations = (await response.json()) as OpenMeteoGeoCodingLocation;
+      const forecast = (await response.json()) as OpenMeteoGeoCodingForecast;
 
-      return locations.results;
+      if (!forecast) {
+        throw new Error("Failed to fetch forecast");
+      }
+
+      return forecast;
     }),
 });
